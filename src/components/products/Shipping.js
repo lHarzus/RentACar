378293d2f22b7d358b78ff4c7126190ebf8957ae
Ruben Rodrigues <rubnitos@hotmail.com>
2023-04-19 15:01:18 +0100

    Loading

--- src/components/products/Shipping.js
+++ src/components/products/Shipping.js
@@ -1,39 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getUser } from "../../actions/auth";
 
-const Shipping = ({
-  auth: {
-    userId,
-    user: { address },
-  },
-  getUser,
-}) => {
+const Shipping = ({ auth: { userId, user }, getUser }) => {
   useEffect(() => {
     getUser(userId);
   }, []);
   return (
     <div>
-      <h3>Shipping Details</h3>
-      <div class="shipping-details">
-        <div className="inline-space-between">
-          <p className="bold">City : </p>
-          <p>{address.city}</p>
-        </div>
-        <div className="inline-space-between">
-          <p className="bold">Street : </p>
-          <p>{address.street}</p>
-        </div>
-        <div className="inline-space-between">
-          <p className="bold">Number : </p>
-          <p>{address.number}</p>
-        </div>
-        <div className="inline-space-between">
-          <p className="bold">Zip : </p>
-          <p>{address.zipcode}</p>
-        </div>
-      </div>
+      {user ? (
+        <Fragment>
+          <h3>Shipping Details</h3>
+          <div class="shipping-details">
+            <div className="inline-space-between">
+              <p className="bold">City : </p>
+              <p>{user.address.city}</p>
+            </div>
+            <div className="inline-space-between">
+              <p className="bold">Street : </p>
+              <p>{user.address.street}</p>
+            </div>
+            <div className="inline-space-between">
+              <p className="bold">Number : </p>
+              <p>{user.address.number}</p>
+            </div>
+            <div className="inline-space-between">
+              <p className="bold">Zip : </p>
+              <p>{user.address.zipcode}</p>
+            </div>
+          </div>
+        </Fragment>
+      ) : (
+        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"></img>
+      )}
     </div>
   );
 };
